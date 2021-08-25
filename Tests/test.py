f55bd024cb1d21193515a098d6cc2c8e62bb978c
AJFlo <36349565+AJFlo@users.noreply.github.com>
2021-08-25 08:49:15 -0500

    Update test.py

--- Tests/test.py
+++ Tests/test.py
@@ -44,8 +44,8 @@ if(myResponse.ok):
     # Loads (Load String) takes a Json file and converts into python data structure (dict or list, depending on JSON)
     jData = json.loads(myResponse.content),
     print(jData), 
-    KVS = jData[0],
-    print(dict(KVS)[Key])
+    print(jData[0]),
+    print(jData[0][0]),
   # If response code is not ok (200), print the resulting http error code with description
     
 
