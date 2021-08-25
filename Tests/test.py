7b142c5c8fc10c052ab56bb4999e00e0cd8aa3d2
AJFlo <36349565+AJFlo@users.noreply.github.com>
2021-08-24 19:41:33 -0500

    Update test.py

--- Tests/test.py
+++ Tests/test.py
@@ -3,6 +3,8 @@ import boto3
 import botocore
 import json
 import typing
+import requests
+from requests.auth import HTTPDigestAuth
 
 running_locally = False
 
@@ -22,14 +24,35 @@ if running_locally:
     )
 else:
     lambda_client = boto3.client('lambda')
+    
+    
+ApiUrl= "https://h2r98egvbi.execute-api.us-east-2.amazonaws.com/Prod/Visitor_Count/"
+    # It is a good practice not to hardcode the credentials. So ask the user to enter credentials at runtime
+myResponse = requests.get(ApiUrl), verify=True)
+#print (myResponse.status_code)
+
+# For successful API call, response code will be 200 (OK)
+if(myResponse.ok):
+
+    # Loading the response data into a dict variable
+    # json.loads takes in only binary or string variables so using content to fetch binary content
+    # Loads (Load String) takes a Json file and converts into python data structure (dict or list, depending on JSON)
+    jData = json.loads(myResponse.content)
+    assert jData > 0
+    assert jData=int(jData)
+
+    print("The response contains {0} properties".format(len(jData)))
+    print("\n")
+    for key in jData:
+        print key + " : " + jData[key]
+else:
+  # If response code is not ok (200), print the resulting http error code with description
+    myResponse.raise_for_status()
 
 
 # Invoke your Lambda function as you normally usually do. The function will run
 # locally if it is configured to do so
 
-LambdaName= "sam-app-StartWithSamFunction02-HLW6Sk6U2FAH"
 
-response = lambda_client.invoke(FunctionName=LambdaName)
 
-assert response > 0
 
