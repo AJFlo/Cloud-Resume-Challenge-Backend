5e1e29189564ff777bb0f7e51a0ca68e12ba6175
AJFlo <36349565+AJFlo@users.noreply.github.com>
2021-08-24 19:50:25 -0500

    Update test.py

--- Tests/test.py
+++ Tests/test.py
@@ -44,7 +44,7 @@ if(myResponse.ok):
     print("The response contains {0} properties".format(len(jData)))
     print("\n")
     for key in jData:
-        print key + " : " + jData[key]
+        print(key + " : " + jData[key])
 else:
   # If response code is not ok (200), print the resulting http error code with description
     myResponse.raise_for_status()
